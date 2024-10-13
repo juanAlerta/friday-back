770fa343b4a2d187ad65636dd49221426630fa86
juanAlerta <metridxbacl@gmail.com>
2024-10-13 13:31:01 +0200

    codigo mal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,103 @@
+mod pub server_struct;
+
+use actix_cors;
+use actix_web_httpauth;
+use dotenv;
+use llm::Model;
+use openssl;
+use rand;
+use serde;
+use serde_json;
+use tokio;
+use tokio_macros;
+
+
+#[actix_web::main]  
+#[cfg(feature = "server")]  
+
 fn main() {
-    println!("Hello, world!")
+    tokio::run(fetch());
+}
+
+async fn fetch() -> std::io::Result<()> {  
+    let config: Config = Config::init();  
+    let model_path = PathBuf::from(&config.llm_model);  
+    let now = std::time::Instant::now();  
+    let model_architecture = match_model_architecture(&config.llm_model_architecture)  
+        .unwrap_or_else(|| {  
+            panic!(  
+                "Failed to find model architecture {} for model: {}.\n",  
+                config.llm_model_architecture, &config.llm_model  
+            );  
+        });  
+
+    let model = llm::load_dynamic(  
+        model_architecture,  
+        &model_path,  
+        Default::default(),  
+        llm::load_progress_callback_stdout,  
+    )  
+    .unwrap_or_else(|err| {  
+        panic!(  
+            "Failed to load {} model from {:?}: {}",  
+            config.llm_model, model_path, err  
+        );  
+    });  
+
+    println!(  
+        "{} model ({}) has been started!\nElapsed: {}ms",  
+        config.llm_model,  
+        config.llm_model_architecture,  
+        now.elapsed().as_millis()  
+    );  
+
+    println!(  
+        "Starting server at https://{}:{}.\n",  
+        config.server_address, config.server_port  
+    );  
+
+    let config: Config = Config::init();  
+    let app_state = web::Data::new(AppState {  
+        model,  
+        config: config.clone(),  
+    });  
+
+    let complete_address = format!("{}:{}", config.server_address, config.server_port);  
+    let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();  
+
+    ssl_builder  
+        .set_private_key_file("certs/key.pem", SslFiletype::PEM)  
+        .unwrap();  
+
+    ssl_builder  
+        .set_certificate_chain_file("certs/cert.pem")  
+        .unwrap();  
+
+    HttpServer::new(move || {  
+        App::new()  
+            .app_data(app_state.clone())  
+            .wrap(middleware::Logger::default())  
+            .wrap(middleware::Logger::new("%a %{User-Agent}i"))  
+            .wrap(middleware::Compress::default())  
+            .wrap(  
+                Cors::default()  
+                    .allowed_origin(&config.allowed_origin)  
+                    .allowed_methods(vec!["GET", "POST"])  
+                    .allowed_headers(vec![  
+                        http::header::AUTHORIZATION,  
+                        http::header::ACCEPT,  
+                        http::header::CONTENT_TYPE,  
+                    ])  
+                    .max_age(config.max_age as usize),  
+            )  
+            .route("/", web::get().to(server_info_handler))  
+            .service(  
+                web::scope("/api")  
+                    .route("/generate", web::post().to(generate_handler))  
+                    .route("/health", web::get().to(health_handler)),  
+            )  
+    })  
+    .bind_openssl(complete_address, ssl_builder)?  
+    .run()  
+    .await 
 }
---
+++ src/server_struct.rs
@@ -0,0 +1,85 @@
+use std::env;
+
+#[derive(Debug, Clone)]  
+pub struct Config {  
+    pub server_address: String,  
+    pub server_port: u16,  
+    pub server_request_timeout: u64,  
+    pub machine_command_timeout: u64,  
+
+    pub max_connections: u32,  
+    pub database_url: String,  
+    pub allowed_origin: String,  
+    pub max_age: u64,  
+
+    pub llm_model: String,  
+    pub llm_model_architecture: String,  
+    pub llm_inference_max_token_count: usize,  
+}  
+
+impl Config {  
+    pub fn init() -> Config {  
+        let _ = dotenv::load();  
+
+        let server_address = env::var("SERVER_ADDRESS")  
+            .expect("SERVER_ADDRESS must be specified")  
+            .parse::<String>()  
+            .unwrap();  
+        let server_port = env::var("SERVER_PORT")  
+            .expect("SERVER_PORT must be specified")  
+            .parse::<u16>()  
+            .unwrap();  
+
+        let max_connections = env::var("MAX_CONNECTIONS")  
+            .expect("MAX_CONNECTIONS must be specified")  
+            .parse::<u32>()  
+            .unwrap();  
+        let database_url = env::var("DATABASE_URL")  
+            .expect("DATABASE_URL must be specified")  
+            .parse::<String>()  
+            .unwrap();  
+        let allowed_origin = env::var("ALLOWED_ORIGIN").expect("ALLOWED_ORIGIN must be specified");  
+        let max_age = env::var("MAX_AGE")  
+            .expect("MAX_AGE must be specified")  
+            .parse::<u64>()  
+            .unwrap();  
+
+        let server_request_timeout = env::var("SERVER_REQUEST_TIMEOUT_IN_SECONDS")  
+            .expect("SERVER_REQUEST_TIMEOUT_IN_SECONDS must be specified")  
+            .parse::<u64>()  
+            .unwrap();  
+        let machine_command_timeout = env::var("MACHINE_COMMAND_TIMEOUT_IN_SECONDS")  
+            .expect("MACHINE_COMMAND_TIMEOUT_IN_SECONDS must be specified")  
+            .parse::<u64>()  
+            .unwrap();  
+
+        let llm_model = env::var("LLM_MODEL")  
+            .expect("LLM_MODEL must be specified")  
+            .parse::<String>()  
+            .unwrap();  
+        let llm_model_architecture = env::var("LLM_MODEL_ARCHITECTURE")  
+            .expect("LLM_MODEL_ARCHITECTURE must be specified")  
+            .parse::<String>()  
+            .unwrap();  
+
+        let llm_inference_max_token_count = env::var("LLM_INFERENCE_MAX_TOKEN_COUNT")  
+            .expect("LLM_INFERENCE_MAX_TOKEN_COUNT must be specified")  
+            .parse::<usize>()  
+            .unwrap();  
+
+        Config {  
+            server_address,  
+            server_port,  
+            server_request_timeout,  
+            machine_command_timeout,  
+            max_connections,  
+            database_url, // used for request history  
+            allowed_origin,  
+            max_age,  
+            // llm specific configuration
+            llm_model,  
+            llm_model_architecture,  
+            llm_inference_max_token_count,  
+        }  
+    }  
+}
